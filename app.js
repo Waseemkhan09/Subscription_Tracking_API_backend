c61ac70c268fc960b8ed99ff5c20f2185ab15615
Waseemkhan09 <waseem7861khan@gmail.com>
2025-08-08 20:20:45 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,39 @@
+import express from 'express';
+
+import { PORT } from './config/env.js'
+
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDatabase from './database/mongodb.js';
+import cookieParser from 'cookie-parser';
+import errorMiddleware from './middlewares/error.middleware.js';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js'
+import workflowRouter from './routes/workflow.routes.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(arcjetMiddleware)
+
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use('/api/v1/workflows', workflowRouter);
+
+app.use(errorMiddleware);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Subscription tracker API!');
+});
+
+app.listen(PORT, async () => {
+  console.log(`Subscription tarcker is running on hhtp://localhost:${PORT}`);
+
+  await connectToDatabase();
+});
+
+export default app;
---
+++ config/env.js
@@ -0,0 +1,16 @@
+import { config } from 'dotenv';
+
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+
+export const { 
+    PORT,
+    NODE_ENV,
+    DB_URI,
+    SERVER_URL,
+    JWT_SECRET,
+    JWT_EXPIRES_IN,
+    ARCJET_KEY,
+    ARCJET_ENV,
+    QSTASH_URL, QSTASH_TOKEN,
+    EMAIL_PASSWORD, 
+  } = process.env;
